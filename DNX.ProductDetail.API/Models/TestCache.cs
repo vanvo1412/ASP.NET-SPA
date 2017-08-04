f53f9b463a3fe27f5ed78f4539415577b2cec339
Van Vo <vanvdse60837@live.com>
2017-08-03 23:27:06 -0700

    first commit

---
+++ DNX.ProductDetail.API/Models/TestCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DNX.ProductDetail.API.Models
+{
+    public partial class TestCache
+    {
+        public string Id { get; set; }
+        public byte[] Value { get; set; }
+        public DateTimeOffset ExpiresAtTime { get; set; }
+        public long? SlidingExpirationInSeconds { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+    }
+}
